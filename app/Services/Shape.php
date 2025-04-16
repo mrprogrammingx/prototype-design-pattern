9f1caab691cca0b562539a36e6addda56f4bfebd
amir <amirjoun2@gmail.com>
2025-04-16 23:12:05 +0400

    Refactored Shape class

--- app/Services/Shape.php
+++ app/Services/Shape.php
@@ -4,11 +4,11 @@ namespace App\Services;
 
 abstract class Shape
 {
-    public int $x;
-    public int $y;
-    public string $color;
-
-    public function __construct()
+    public function __construct(public int $x, public int $y, public string $color)
     {
+        $this->x = $x;
+        $this->y = $y;
+        $this->color = $color;
     }
+    
 }
