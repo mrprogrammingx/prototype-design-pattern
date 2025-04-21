9b1541a98b66dfde21b6bb7142748522173ae131
amir <amirjoun2@gmail.com>
2025-04-21 18:08:06 +0400

    Refactored Rectangle class

--- app/Services/Rectangle.php
+++ app/Services/Rectangle.php
@@ -2,9 +2,16 @@
 
 namespace App\Services;
 
-class Rectangle
+use App\Services\Shape;
+
+class Rectangle extends Shape
 {
     public function __construct(public int $width, public int $height)
     {
+    }
+
+    public function clone(): self
+    {
+        return new Rectangle($this->width, $this->height);
     }
 }
