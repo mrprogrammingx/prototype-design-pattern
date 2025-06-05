17ff65dc13fbcb66b96342baccbeed80565cccb3
amir <amirjoun2@gmail.com>
2025-06-05 23:11:10 +0400

    Added getShapesByHeight method in Application class

--- app/Services/Application.php
+++ app/Services/Application.php
@@ -139,5 +139,16 @@ class Application
         }
         return $filteredShapes;
     }
-    
+
+    public function getShapesByHeight(int $height): array
+    {
+        $filteredShapes = [];
+        foreach ($this->shapes as $shape) {
+            if ($shape instanceof Rectangle && $shape->getHeight() === $height) {
+                $filteredShapes[] = $shape;
+            }
+        }
+        return $filteredShapes;
+    }
+
 }
